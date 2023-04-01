397778154a9aa5acea5e4d1ec1a3a923b4eb297c
JMacalaguing <125894320+JMacalaguing@users.noreply.github.com>
2023-04-01 20:52:04 +0800

    Simple Calculator
    JavaScript-exercise Quiz

---
+++ index.js
@@ -0,0 +1,40 @@
+
+let button = document.getElementById('add').addEventListener('click', add).getElementById("times").addEventListener("click", times);
+button =document.getElementById("minus").addEventListener("click", minus);
+button =document.getElementById("divide").addEventListener("click", divide);
+
+function add () {
+  
+    let num1= Number(document.getElementById("NumberOne").value);
+    let num2 = Number (document.getElementById("Numbertwo").value);
+    let result = num1+num2;
+     answer = document.getElementById("answer")
+     answer.innerHTML = result;
+    }
+
+function times () {
+  
+    let num1= Number(document.getElementById("NumberOne").value);
+    let num2 = Number (document.getElementById("Numbertwo").value);
+    let result = num1*num2;
+    answer = document.getElementById("answer")
+    answer.innerHTML = result;
+     }
+function minus () {
+  
+    let num1= Number(document.getElementById("NumberOne").value);
+    let num2 = Number (document.getElementById("Numbertwo").value);
+    let result = num1-num2;
+    answer = document.getElementById("answer")
+    answer.innerHTML = result;
+     }
+function divide (){
+    let num1= Number(document.getElementById("NumberOne").value);
+    let num2 = Number (document.getElementById("Numbertwo").value);
+    let result = num1/num2;
+    answer = document.getElementById("answer")
+    answer.innerHTML = result;
+}
+  
+  
+
